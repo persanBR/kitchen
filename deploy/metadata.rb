fb6379ae12cccb3b8ea5ea43b6933bf186fc2e60
Alan Pereira <alan.pereira@outsystems.com>
2022-01-30 20:54:21 +0000

    create deploy recipe

---
+++ deploy/metadata.rb
@@ -0,0 +1,9 @@
+name        "Deploy Application"
+description "Deploy Application"
+maintainer  "ASP"
+license     "Apache 2.0"
+version     "1.0.0"
+
+recipe "deploy::app", "Nodejs deploy"
+
+##PS.: There should be more details here;
---
+++ deploy/recipes/app.rb
@@ -0,0 +1,25 @@
+##PS.: The app should not run as root for security reassons
+##PS.: This should be modularized, this recipe is doing too much stuff
+
+bash 'app_setup' do
+  user 'root'
+  cwd '/root'
+  code <<-EOH
+    ##smelly code
+    kill $(pidof node)
+    cd ./node-3tier-app/
+    git pull
+    HOST=`hostname | grep -i frontend`
+    if [ -z "$HOST" ]
+    then
+            cd ./api
+    else
+            cd ./web
+    fi
+    mkdir ./log
+    npm config set registry http://registry.npmjs.org/  
+    npm config set strict-ssl false
+    npm install
+    nohup npm start &
+  EOH
+end
